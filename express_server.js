cb5c32134dfcaf8ef5e048405fa610d6483b22c7
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-28 00:29:44 +0000

    added the random string generator, may come back to implement a RSG logic with less lines of code

--- express_server.js
+++ express_server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; 
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+
+function generateRandomString(length) {
+
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+/*generates a random string of 6 charcters console.log(generateRandomString(6)); 
+*/
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -33,6 +48,14 @@ app.get("/urls/:shortURL", (req, res) => {
   };
 
   res.render("urls_show", templateVars);
+
+});
+
+app.post("/urls", (req, res) => {
+  
+  console.log(req.body);  // Log the POST request body to the console
+  
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 
 });
 
